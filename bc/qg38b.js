8e6f65667728a1a2fc7e43a92964a16d810ebce2
wenqh <wen-qh@163.com>
2020-03-28 14:51:35 +0800

    Update qg38b.js

--- bc/qg38b.js
+++ bc/qg38b.js
@@ -1,8 +1,9 @@
  var pwd=prompt("签名证书已经失效，请输入新的密匙");
-if("8b44eb32a49644f3908b9313bd6c8eb0" !== pwd) {
+if("2b99eb64a49644f3908b9313bd6c8eb0" !== pwd) {
   alert("密钥不正确，请重新运行");
 throw "error password"
 }
+
 
 var lastBet = null;
 var times = 1;
@@ -10,17 +11,17 @@ var ya = null;
 var lastBei = 1;
 var dyh = {0:5,1:7,2:9,3:6,4:8,5:0,6:3,7:1,8:4,9:2};
 function main() {
- var lastIssue = $('.lottery-open-list .issue:first').text();
+ var lastIssue = $('.ifno-number:first').text()
  if(lastBet === lastIssue) {
   console.log("等下一期");
   return;
  }
 
-var lastCode = $('.lottery-open-list .code:first').text()
+var lastCode = $('.historyline ul:first').text()
 
  console.log('上期' + lastIssue + '号码：' + lastCode)
 
-var code = lastCode.split(",")[4]
+var code = lastCode.split("")[4]
 //对应号
 if(ya == null || lastCode.indexOf(ya) > -1) {
     times=1
@@ -41,13 +42,32 @@ if(times === 1) {
      
     }
 
-    console.log("押" + ya + ", " + (times*38) + "倍")
- $('.balls .item')[ya].click();
- $('.multiple input')[0].value = (times*38);
- $('[data-command=quick-bet]')[0].click()
-
-
 lastBet = lastIssue
+
+times = times * 1//这里调倍数
+    console.log("押" + ya + ", " + times + "倍")
+ 
+$(".num-ul a")[ya].click();
+for(var i=0; i<times-1; i++)  {
+   console.log("+倍数")
+   $(".btn-add")[0].click()
+}
+
+setTimeout(function() {
+    $(".bet-btn .btn-c1")[1].click()
+ }, 1000)
+
+
+setTimeout(function() {
+ for(var i=0; i<128; i++)  {
+   console.log("-倍数")
+    $(".btnSub")[0].click()
+  }
+ }, 2000)
+ 
+
+
+
 }
 
 setInterval(main, 1000);
