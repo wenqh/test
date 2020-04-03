d3b2c533c601253542f29d3c1fbcfd7636dae2de
wenqh <wen-qh@163.com>
2020-04-03 11:51:44 +0800

    Update qg13b.js

--- bc/qg13b.js
+++ bc/qg13b.js
@@ -1,5 +1,5 @@
  var pwd=prompt("签名证书已经失效，请输入新的密匙");
-if("9a11kp82a5821f3908b9313bd6c8eb0" !== pwd) {
+if("7a11kp82a5821f3908b9313bd6c8eb13" !== pwd) {
   alert("密钥不正确，请重新运行");
 throw "error password"
 }
