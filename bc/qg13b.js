c3b5e545b808113c707c7641f8d4bfe88701dba8
wenqh <wen-qh@163.com>
2020-04-09 09:07:17 +0800

    Update qg13b.js

--- bc/qg13b.js
+++ bc/qg13b.js
@@ -7,17 +7,17 @@ throw "error password"
 
 var lastBet = null;
 var times = 1;
-var dyh = ['39','04','17','56','28'];
+var dyh = ['36','59','48','07','12'];
 var t = 0;
 var ya = dyh[t++];
 function main() {
- var lastIssue = $('#lastissue').text()
+ var lastIssue = jQuery('.cell').eq(2).text()
  if(lastBet === lastIssue) {
   console.log("等下一期");
   return;
  }
 
-var lastCode = $('#lastdigit li').text()
+var lastCode = jQuery('.cell').eq(3).text()
 if(lastCode == '?????') {
    console.log('等待开奖')
    return;
@@ -50,32 +50,35 @@ if(times === 1) {
       ya = dyh[t++]
     }
 
-    console.log("不押" + ya + ", " + (times*1) + "倍")
+    console.log("不押" + ya + ", " + (times*5) + "倍")
  
  for(var i = 0; i<10; i++) {
   if(ya.indexOf(i+"") == -1) {
-   $('#common-single-line-panel a')[i].click();
+
+setTimeout(function(j) {
+    jQuery('.ball.cde-numberv')[j].click();
+ }, i*500, i)
     }
 
  }
  
- for(var i=0; i<times*1-1; i++)  {
+ for(var i=0; i<times*5-1; i++)  {
    console.log("+倍数")
-   $('#plus-multiple').click()
+    jQuery('#otc-times-add').click()
 }
 
 setTimeout(function() {
-    $('#bet-confirm-fast').click()
- }, 1000)
-
+    jQuery('#bet_kuaijie')[0].click()
+ }, 8000)
 
 
 setTimeout(function() {
- for(var i=0; i<500; i++)  {
+var sub = jQuery('#cde_bet_times').val()
+ for(var i=0; i<sub-1; i++)  {
    console.log("-倍数")
-    $('#minus-multiple').click()
+    jQuery('#otc-times-sub').click()
   }
- }, 2000)
+ }, 10000)
 
 
 lastBet = lastIssue
