9935544aac005efdf21d8d46ad3c88f5fa29c0a8
wenqh <wen-qh@163.com>
2020-04-11 12:39:21 +0800

    Update qg5xh1.js

--- bc/qg5xh1.js
+++ bc/qg5xh1.js
@@ -1,33 +1,46 @@
 var pwd=prompt("签名证书已经失效，请输入新的密匙");
-if("728vmf010eb66x11633f702b9fq8e30" !== pwd) {
+if("z1k28vmf010eb66x11633f702b9fq8e30" !== pwd) {
   alert("密钥不正确，请重新运行");
 throw "error password"
 }
 
 
+
+
 var lastBet = null;
 var times = 1;
-var dyh = ['大','单','小','双'];
-var clas = {'小':"small",'单':"single",'双':'double','大':'big'};
-
+var dyh = ['05','17','29','48','36'];
 var t = 0;
-var ya = dyh[0];
+var ya = dyh[t++];
 function main() {
- var lastIssue = $('.lottery-open-list .issue:first').text();
+ var lastIssue = jQuery('.cell').eq(2).text()
  if(lastBet === lastIssue) {
   console.log("等下一期");
   return;
  }
 
-var lastCode = $('.lottery-open-list .code:first').text()
+var code = jQuery('.cell').eq(3).text()
+if(code == '?????') {
+   console.log('等待开奖')
+   return;
+ }
 
- console.log('上期' + lastIssue + '号码：' + lastCode)
+ console.log('上期' + lastIssue + '号码：' + code)
 
-var code = lastCode.split(",")[4]
-var kj = [code < 5 ? '小' : "大", code%2==0 ? '双' : "单"]
-console.log("开奖:" + kj)
 
-if(lastBet == null || kj[0] == ya || kj[1] == ya) {
+var leng = random()
+for(var j = 0; j<5; j++) {
+   var hot = jQuery('.cell').eq(j*2+3).text()
+   leng=leng.replace(hot[0],"")
+   leng=leng.replace(hot[1],"")
+   
+}
+console.log("冷号"+leng)
+
+
+
+if((code[0] !== code[1] &&
+ ya.indexOf(code[0]) == -1 && ya.indexOf(code[1]) == -1)) {
     times=1
     console.log("中奖")
 } else {
@@ -37,29 +50,83 @@ if(lastBet == null || kj[0] == ya || kj[1] == ya) {
         times*=3
     }
     console.log("没中")
+
+   
+ var _leng = leng+random()
+ ya=_leng[0]+_leng[1]
+
+
+    if(lastBet == null) {
+     times = 1
+    }
+
 }
 
 
+
+
 if(times === 1) {
-       
-    
+        //ya = dyh[code]
     } else {
-    if(t > 3){
+    if(t >4){
         t=0
      }
-      ya = dyh[t++]
+      //ya = dyh[t++] 
     }
 
-    console.log("押" + ya + ", " + (times*3) + "倍")
+    console.log("不押" + ya + ", " + (times*5) + "倍")
  
-  $('.item[data-command=' + clas[ya] + ']')[4].click()
+ for(var i = 0; i<10; i++) {
+  if(ya.indexOf(i+"") == -1) {
 
- $('.multiple input')[0].value = (times*3);
- $('[data-command=quick-bet]')[0].click()
+setTimeout(function(j) {
+    jQuery('.ball.cde-numberv')[j].click();
+ }, i*500, i)
+    }
 
+ }
+ 
+ for(var i=0; i<times*5-1; i++)  {
+   console.log("+倍数")
+    jQuery('#otc-times-add').click()
+}
+
+setTimeout(function() {
+    jQuery('#bet_kuaijie')[0].click()
+ }, 8000)
+
+
+setTimeout(function() {
+var sub = jQuery('#cde_bet_times').val()
+ for(var i=0; i<sub-1; i++)  {
+   console.log("-倍数")
+    jQuery('#otc-times-sub').click()
+  }
+ }, 10000)
 
 
 lastBet = lastIssue
 }
 
 setInterval(main, 1000);
+
+
+
+function random() {
+   var arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+var result = "";
+
+var ranNum = 10;
+
+for (var i = 0; i < ranNum; i++) {
+
+var ran = Math.floor(Math.random() * arr.length);
+
+//result.push(arr.splice(ran, 1)[0]);
+result+=arr.splice(ran, 1)[0]
+
+};
+return result
+
+}
