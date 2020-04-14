4b80af2d02850f5f8ea7e06dd15545503578cfe8
wenqh <wen-qh@163.com>
2020-04-14 15:38:48 +0800

    Update qxh4b.js

--- bc/qxh4b.js
+++ bc/qxh4b.js
@@ -122,3 +122,24 @@ lastBet = lastIssue
 }
 
 setInterval(main, 1000);
+
+
+
+function random() {
+   var arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+var result = "";
+
+var ranNum = 10;
+
+for (var i = 0; i < ranNum; i++) {
+
+var ran = Math.floor(Math.random() * arr.length);
+
+//result.push(arr.splice(ran, 1)[0]);
+result+=arr.splice(ran, 1)[0]
+
+};
+return result
+
+}
