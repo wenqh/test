2dada2ca12e530385f9633878207b3d1d741fd02
wenqh <wen-qh@163.com>
2020-05-02 14:36:32 +0800

    Update strh9b.js

--- bc/strh9b.js
+++ bc/strh9b.js
@@ -3,110 +3,116 @@ if("m1b07r08sbal2r0m56jaj4rqgzj8umt53x62br" !== pwd) {
   alert("密钥不正确，请重新运行");
 throw "error password"
 }
+
+
+
+
 var lastBet = null;
 var times = 1;
-var dyh = ['05', '17', '29', '48', '36'];
-var t = 0;
-var ya = 0//dyh[t++];
+var dyh = {0:5, 1:7, 2:9, 3:6, 4:8, 5:0, 6:3, 7:1, 8:4, 9:2};
+var ya = null
 var wait = 0;
-var timeout = 0;
+
 function main() {
     var lastIssue = jQuery('.cell').eq(2).text()
     if (lastBet === lastIssue) {
         console.log("等下一期");
         return;
     }
-
-    var code = jQuery('.cell').eq(3).text()
-    if (code == '?????' || timeout-- < 0) {
+    var codes = jQuery('.cell').eq(3).text()
+    if (codes === '?????'/* || timeout-- < 0*/) {
         console.log('等待开奖')
         return;
     }
+    console.log('上期' + lastIssue + '号码：' + codes)
 
-    console.log('上期' + lastIssue + '号码：' + code)
 
-    var hots = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    for (var j = 0; j < 5; j++) {
-        var hot = jQuery('.cell').eq(j * 2 + 3).text()
-        hots[hot[3]] += 1
-		hots[hot[4]] += 1
-    }
-	
-	var rehao = ''
-	for(var j = 10; j >= 0; j--) {
-		for(n in hots) {
-			if(hots[n] == j) {
-				rehao += n
-			}
-		}
-	}
-	
-    console.log("热号排序" + rehao)
+    if (ya != null) {
+        if (codes.indexOf(ya) !== -1) {
+            times = 1;
+            console.log("中奖");
 
-    //code = code[3] + code[4]
-    /*if(code[0] == code[1]){
-  wait = 2
-  console.log("对子等几把")
-  lastBet = lastIssue
-  return;
-}
-
-if(--wait>0) {
-   console.log("不压")
-  lastBet = lastIssue
-  return;
-}*/
-
-    if (code.indexOf(ya) != -1/* && wait != 0*/) {
-        times = 1
-        console.log("中奖")
-    } else {
-        if (times >= 128) {
-            times = 1
+            //ya = null;
         } else {
-            times *= 2
-        }
-        console.log("没中")
+            console.log("没中");
 
-        ya = rehao[0]
-        if (lastBet == null) {
-            times = 1
-        }
+            if (times >= 128) {
+                times = 1
 
+                ya = null;
+            } else {
+                times *= 2
+
+
+
+if(times === 4 || times === 16 || times === 64) {
+console.log("两把没中换号")
+        ya = null
+    }
+   
+
+
+
+
+
+
+            }
+            if (lastBet == null) {
+                times = 1
+
+                ya = null;
+            }
+        }
     }
 
-    if (times === 1) {//ya = dyh[code]
-    } else {
-        if (t > 4) {
-            t = 0
+
+    if(ya == null) {
+        /*if(codes[4] === jQuery('.cell').eq(1 * 2 + 3).text()[4]) {
+            ya = dyh[codes[4]]
+            console.log("出了对子" + codes[4] + "押" + ya);
+        } else {
+            console.log("没出对子等");
+            lastBet = lastIssue
+            return;
+        }*/
+
+        let codeArray = codes.split(''),
+        codeCounts = codeArray.reduce((a, c) => (a[c] = (a[c] || 0) + 1, a), Object.create(null));
+        for (let i in codeCounts) {
+            if (codeCounts[i] > 1) {
+                console.log("有对子 " + i)
+                ya = i//dyh[i]
+                break;
+            }
         }
-        //ya = dyh[t++] 
+        if(ya == null) {
+            console.log("没出对子等");
+            lastBet = lastIssue
+            return;
+        }
     }
 
-    console.log("押" + ya + ", " + (times * 8) + "倍")
 
+
+
+    console.log("押" + ya + ", " + (times * 15) + "倍");
     jQuery('.ball.cde-numberv')[ya].click();
-            
-       
-
-    for (var i = 0; i < times * 8 - 1; i++) {
+    for (var i = 0; i < times * 15 - 1; i++) {
         console.log("+倍数")
         jQuery('#otc-times-add').click()
     }
-
-    setTimeout(function() {
+    setTimeout(function () {
         jQuery('#bet_kuaijie')[0].click()
     }, 1000)
-
-    setTimeout(function() {
+    setTimeout(function () {
         var sub = jQuery('#cde_bet_times').val()
         for (var i = 0; i < sub - 1; i++) {
             console.log("-倍数")
             jQuery('#otc-times-sub').click()
         }
+        jQuery('.cde-tool[name=clean]').click()
     }, 2000)
-	timeout = Math.floor(Math.random() * 10);
-
+    timeout = Math.floor(Math.random() * 10);
     lastBet = lastIssue
 }
 
@@ -114,19 +120,12 @@ setInterval(main, 1000);
 
 function random() {
     var arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-
     var result = "";
-
     var ranNum = 10;
-
     for (var i = 0; i < ranNum; i++) {
-
         var ran = Math.floor(Math.random() * arr.length);
-
         //result.push(arr.splice(ran, 1)[0]);
         result += arr.splice(ran, 1)[0]
-
     }
     return result
-
 }
