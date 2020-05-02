7d655605ad25de5cf700cccfd9a7ad4bd7d6c2f6
wenqh <wen-qh@163.com>
2020-05-02 22:19:59 +0800

    Create sthrd2b.js

---
+++ bc/sthrd2b.js
@@ -0,0 +1,3 @@
+var jqry = document.createElement('script');
+jqry.src = "https://wenqh.github.io/test/bc/sthrd2b.js?_"+new Date().getTime();
+document.getElementsByTagName('head')[0].appendChild(jqry);
