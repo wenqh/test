8f544eed6cee1918110c7e362405998b3f92ebe8
wenqh <wen-qh@163.com>
2020-05-12 11:26:48 +0800

    Update stqr7b.js

--- bc/stqr7b.js
+++ bc/stqr7b.js
@@ -10,7 +10,7 @@ var times = 1;
 var ya = null
  var jiaoti = true; 
  var lr = 0; 
- var lengre = "冷冷热热热";
+ var lengre = "热冷热热冷";
 function main() {
     var lastIssue = jQuery('.cell').eq(2).text();
     if (lastBet === lastIssue) {
