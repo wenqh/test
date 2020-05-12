948f4ebd5e5f6272a9936d4131e6aacc8db29d8d
wenqh <wen-qh@163.com>
2020-05-12 11:06:38 +0800

    Update stqr7b.js

--- bc/stqr7b.js
+++ bc/stqr7b.js
@@ -5,130 +5,226 @@ if ("020v21t5kl5h6gt02i3o0n10s1kh0dl01ib7j3nbr5v26kiz" !== pwd) {
 }
 
 
-
 var lastBet = null;
 var times = 1;
-var dyh = {0:5, 1:7, 2:9, 3:6, 4:8, 5:0, 6:3, 7:1, 8:4, 9:2};
 var ya = null
-var wait = 0;
-
+ var jiaoti = true; 
+ var lr = 0; 
+ var lengre = "冷冷热热热";
 function main() {
-    var lastIssue = jQuery('.cell').eq(2).text()
+    var lastIssue = jQuery('.cell').eq(2).text();
     if (lastBet === lastIssue) {
         console.log("等下一期");
         return;
     }
-    var codes = jQuery('.cell').eq(3).text()
-    if (codes === '?????'/* || timeout-- < 0*/) {
-        console.log('等待开奖')
+    var codes = jQuery('.cell').eq(3).text();
+    if (codes == '?????') {
+        console.log('等待开奖');
         return;
     }
     console.log('上期' + lastIssue + '号码：' + codes)
-
-
-    if (ya != null) {
-        if (codes.indexOf(ya) !== -1) {
-            times = 1;
-            console.log("中奖");
-
-            //ya = null;
-        } else {
-            console.log("没中");
-
-            if (times >= 128) {
-                times = 1
-
-                ya = null;
-            } else {
-                times *= 2
-
-
-
-if(times === 4 || times === 16 || times === 64) {
-console.log("两把没中换号")
-        ya = null
+    var hots6 = ''
+    for (var j = 0; j < 10; j++) {
+        var c = jQuery('.cell').eq(j * 2 + 3).text()[4]
+        hots6 = hots6.indexOf(c) === -1 ? hots6 + c : hots6
     }
-   
 
-
-
-
-
-
-            }
-            if (lastBet == null) {
-                times = 1
-
-                ya = null;
-            }
+    var rand = random()
+    for (var r in rand) {
+        if (hots6.indexOf(rand[r]) === -1) {
+            //console.log("不足6个补" + rand[r])
+            hots6 += rand[r]
+        }
+    }
+    hots6 = hots6.substring(0, 6)
+    var leng6 = ""
+    for (var n = 0; n<10; n++) {
+        if(hots6.substring(0, 4).indexOf(n+"")===-1) {
+           leng6 += n
         }
     }
 
 
-    if(ya == null) {
-        /*if(codes[4] === jQuery('.cell').eq(1 * 2 + 3).text()[4]) {
-            ya = dyh[codes[4]]
-            console.log("出了对子" + codes[4] + "押" + ya);
+    if (ya != null && ya.indexOf(codes[4]) !== -1) {
+        times = 1;
+        console.log("中奖")
+    } else {
+        if (times >= 81 || ya == null) {
+            times = 1
         } else {
-            console.log("没出对子等");
-            lastBet = lastIssue
-            return;
+            times *= 3
+        }
+        console.log("没中")
+
+
+       if (lengre[(lr++)%lengre.length] === "热") { 
+          ya = hots6; 
+          console.log('压热') 
+        } else { 
+          ya = leng6; 
+          console.log('压冷') 
+        }
+ 
+ console.log("热号"+hots6+"冷号"+leng6)
+    
+    }
+
+    console.log("押" + ya + ", " + (times *12) + "倍")
+
+    for (var i in ya) {
+        setTimeout(function(j) {
+            jQuery('.num-list.num-list-rac').eq(4).find('span')[j].click();
+        }, i * 500, ya[i])
+    }
+
+    /*for (var i = 0; i < times * 1 - 1; i++) {
+        console.log("+倍数");
+        jQuery('#otc-times-add').click()
+    }*/
+ 
+ setKeywordText(jQuery('#cde_bet_times')[0], times*12)
+    setTimeout(function() {
+        jQuery('#bet_kuaijie')[0].click()
+    }, 5000);
+    setTimeout(function() {
+        /*var sub = jQuery('#cde_bet_times').val();
+        for (var i = 0; i < sub - 1; i++) {
+            console.log("-倍数");
+            jQuery('#otc-times-sub').click()
         }*/
+        jQuery('.cde-tool[name=clean]').click();
+    }, 7000)
+    lastBet = lastIssue
+}
+setInterval(main, 1000);
+//main()
+function random() {
+    var arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    var result = "";
+    var ranNum = 10;
+    for (var i = 0; i < ranNum; i++) {
+        var ran = Math.floor(Math.random() * arr.length);
+        //result.push(arr.splice(ran, 1)[0]); 
+        result += arr.splice(ran, 1)[0]
+    }
+    return result
+}
 
-        let codeArray = codes.split(''),
-        codeCounts = codeArray.reduce((a, c) => (a[c] = (a[c] || 0) + 1, a), Object.create(null));
-        for (let i in codeCounts) {
-            if (codeCounts[i] > 1) {
-                console.log("有对子 " + i)
-                ya = i//dyh[i]
-                break;
-            }
+
+function setKeywordText(e, text) { 
+ e.value = text; var evt = document.createEvent("Events");
+ evt.initEvent("input", true, true);
+ e.dispatchEvent(evt);
+ }var lastBet = null;
+var times = 1;
+var ya = null
+ var jiaoti = true; 
+ var lr = 0; 
+ var lengre = "冷冷热热热";
+function main() {
+    var lastIssue = jQuery('.cell').eq(2).text();
+    if (lastBet === lastIssue) {
+        console.log("等下一期");
+        return;
+    }
+    var codes = jQuery('.cell').eq(3).text();
+    if (codes == '?????') {
+        console.log('等待开奖');
+        return;
+    }
+    console.log('上期' + lastIssue + '号码：' + codes)
+    var hots6 = ''
+    for (var j = 0; j < 10; j++) {
+        var c = jQuery('.cell').eq(j * 2 + 3).text()[4]
+        hots6 = hots6.indexOf(c) === -1 ? hots6 + c : hots6
+    }
+
+    var rand = random()
+    for (var r in rand) {
+        if (hots6.indexOf(rand[r]) === -1) {
+            //console.log("不足6个补" + rand[r])
+            hots6 += rand[r]
         }
-        if(ya == null) {
-            console.log("没出对子等");
-            lastBet = lastIssue
-            return;
+    }
+    hots6 = hots6.substring(0, 6)
+    var leng6 = ""
+    for (var n = 0; n<10; n++) {
+        if(hots6.substring(0, 4).indexOf(n+"")===-1) {
+           leng6 += n
         }
     }
 
 
+    if (ya != null && ya.indexOf(codes[4]) !== -1) {
+        times = 1;
+        console.log("中奖")
+    } else {
+        if (times >= 81 || ya == null) {
+            times = 1
+        } else {
+            times *= 3
+        }
+        console.log("没中")
 
 
-    console.log("押" + ya + ", " + (times * 2) + "倍");
-      for (var i = 0; i < 5; i++) {
-        setTimeout(function (j) {
-            jQuery('.num-list.num-list-rac').eq(j).find('span')[ya].click();
-        }, i * 500, i)
-
+       if (lengre[(lr++)%lengre.length] === "热") { 
+          ya = hots6; 
+          console.log('压热') 
+        } else { 
+          ya = leng6; 
+          console.log('压冷') 
+        }
+ 
+ console.log("热号"+hots6+"冷号"+leng6)
+    
     }
 
-   for (var i = 0; i < times * 2 - 1; i++) { 
- console.log("+倍数") 
- jQuery('#otc-times-add').click() 
- } 
-   setTimeout(function() { 
- jQuery('#bet_kuaijie')[0].click() 
- }, 5000) 
-   setTimeout(function() { 
- var sub = jQuery('#cde_bet_times').val() 
- for (var i = 0; i < sub - 1; i++) { 
- console.log("-倍数") 
- jQuery('#otc-times-sub').click() 
- } 
- jQuery('.cde-tool[name=clean]').click();
- }, 7000) 
-   timeout = Math.floor(Math.random() * 10); 
-   lastBet = lastIssue 
- } 
-   setInterval(main, 1000); 
-   function random() { 
- var arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]; 
-   var result = ""; 
-   var ranNum = 10; 
-   for (var i = 0; i < ranNum; i++) { 
-   var ran = Math.floor(Math.random() * arr.length); 
-   //result.push(arr.splice(ran, 1)[0]); 
- result += arr.splice(ran, 1)[0] 
-   } 
- return result 
-   } 
+    console.log("押" + ya + ", " + (times *12) + "倍")
+
+    for (var i in ya) {
+        setTimeout(function(j) {
+            jQuery('.num-list.num-list-rac').eq(4).find('span')[j].click();
+        }, i * 500, ya[i])
+    }
+
+    /*for (var i = 0; i < times * 1 - 1; i++) {
+        console.log("+倍数");
+        jQuery('#otc-times-add').click()
+    }*/
+ 
+ setKeywordText(jQuery('#cde_bet_times')[0], times*12)
+    setTimeout(function() {
+        jQuery('#bet_kuaijie')[0].click()
+    }, 5000);
+    setTimeout(function() {
+        /*var sub = jQuery('#cde_bet_times').val();
+        for (var i = 0; i < sub - 1; i++) {
+            console.log("-倍数");
+            jQuery('#otc-times-sub').click()
+        }*/
+        jQuery('.cde-tool[name=clean]').click();
+    }, 7000)
+    lastBet = lastIssue
+}
+setInterval(main, 1000);
+//main()
+function random() {
+    var arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    var result = "";
+    var ranNum = 10;
+    for (var i = 0; i < ranNum; i++) {
+        var ran = Math.floor(Math.random() * arr.length);
+        //result.push(arr.splice(ran, 1)[0]); 
+        result += arr.splice(ran, 1)[0]
+    }
+    return result
+}
+
+
+function setKeywordText(e, text) { 
+ e.value = text; var evt = document.createEvent("Events");
+ evt.initEvent("input", true, true);
+ e.dispatchEvent(evt);
+ }
+
+   
