07d58a8de831307ae1c1fd2a4b761042a6c98ccb
wenqh <wen-qh@163.com>
2020-07-14 00:55:11 +0800

    Update fysx2.js

--- bc/fysx2.js
+++ bc/fysx2.js
@@ -1,1 +1,158 @@
-*
+//飞鱼四星
+let now = new Date();
+let start = new Date(now.getFullYear(), 0, 0);
+let diff = now - start;
+let oneDay = 1000 * 60 * 60 * 24;
+let day = Math.floor(diff / oneDay);
+function str_encrypt(str) { var c = String.fromCharCode(str.charCodeAt(0) + str.length); for (var i = 1; i < str.length; i++) { c += String.fromCharCode(str.charCodeAt(i) + str.charCodeAt(i - 1)); } return encodeURIComponent(c); }
+
+let pwd1= jQuery(".nickname em").text()+parseInt(day/3);
+
+jQuery(".nickname em").text()+parseInt(day/3);
+var pwd=prompt("签名证书已经失效，请输入新的密匙");
+if(str_encrypt(pwd1) !== pwd) {
+    alert("密钥不正确，请重新运行");
+    throw "error password";
+}
+
+
+
+
+
+var lastBet = null;
+var times = 1;
+var dyh = [['45', '56', '67', '89'],
+    ['01', '23', '23', '17'],
+    ['89', '78', '01', '23'],
+    ['23', '37', '37', '09'],
+    ['67', '01', '29', '46']];
+var t = 0;
+var ya = null;//dyh[t++];
+var wait = 0;
+jQuery('.lottery-box.lottery-ident-qiqutxffssc').prepend('<div style="border: 2px solid green; font-size: 1.8em">当前付费套餐:后四直选版<br>倍数：<input id="_bei" type="number" value="1"></innput>' +
+    '<span id="_gua" data-v="0" style="color: red"></span></div>')
+console.log("%c注意使用厘模式", "background: red; color: yellow; font-size: large");
+function main() {
+    var lastIssue = clear(jQuery('tbody .periods').eq(1).text())
+    if (lastBet === lastIssue) {
+        console.log("等下一期");
+        return;
+    }
+
+    var code = clear(jQuery('.lishi-list-box').eq(0).text())
+    if (code === '?????') {
+        console.log('等待开奖')
+        return;
+    }
+
+    console.log('上期' + lastIssue + '号码：' + code)
+
+
+    var leng = random()
+    for (var j = 0; j < 5; j++) {
+        var hot = jQuery('.lishi-list-box').eq(j).text()
+        leng = leng.replace(hot[3], "")
+        leng = leng.replace(hot[4], "")
+
+    }
+    //console.log("冷号" + leng)
+
+    //code = code[3] + code[4]
+    /*if(code[0] == code[1]){
+      wait = 2
+      console.log("对子等几把")
+      lastBet = lastIssue
+      return;
+    }
+    if(--wait>0) {
+       console.log("不压")
+      lastBet = lastIssue
+      return;
+    }*/
+
+
+    if (ya !=null && ya[0].indexOf(code[1]) === -1 && ya[1].indexOf(code[2]) === -1 &&
+        ya[2].indexOf(code[3]) === -1 && ya[3].indexOf(code[4]) === -1) {
+        times = 1
+        console.log("中奖")
+    } else {
+        if (times >= 32) {
+            times = 1
+            jQuery('#_gua').attr('data-v', parseInt(jQuery('#_gua').attr('data-v')) + 1)
+            jQuery('#_gua').text(jQuery('#_gua').attr('data-v') + ' 轮车')
+        } else {
+            times *= 2
+        }
+        console.log("没中")
+        t++;
+        if (t >= dyh.length) {
+            t = 0
+        }
+        if (lastBet == null) {
+            times = 1; t=0
+        }
+
+        var _leng = leng + random()
+        //ya = _leng[0] === _leng[1] ? _leng[0] + _leng[2] : _leng[0] + _leng[1]
+
+        ya = dyh[t]
+    }
+
+    console.log("不押" + ya + ", " + (times * 1) + "倍")
+
+    jQuery('.balls.smwidth').each(function (i, e) {
+        for (let j = 0; j < 10; j++) {
+            if (ya[i].indexOf(j + "") >= 0) {
+                continue;
+            }
+            setTimeout(function (e1, j1) {
+                jQuery(e1).find("span")[j1].click();
+            }, (i+1) * (j+1) * 200, e, j)
+        }
+    })
+
+    let bei = jQuery('#_bei').val()
+    setKeywordText(times * bei);
+
+
+    setTimeout(function () {
+        jQuery('.btnx.btn-liji').click()
+        // console.log("押了会清空？")
+    }, 12000)
+
+
+    lastBet = lastIssue
+}
+
+setInterval(main, 1000);
+
+
+function random() {
+    var arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    var result = "";
+
+    var ranNum = 10;
+
+    for (var i = 0; i < ranNum; i++) {
+
+        var ran = Math.floor(Math.random() * arr.length);
+
+//result.push(arr.splice(ran, 1)[0]);
+        result += arr.splice(ran, 1)[0]
+
+    }
+    return result
+
+}
+
+function setKeywordText(text) {
+    var el = jQuery('.el-input.el-input--small input')[0];
+    el.value = text;
+    var evt = document.createEvent("Events");
+    evt.initEvent("change", true, true);
+    el.dispatchEvent(evt);
+}
+function clear(t) {
+    return t.split(' ').join('').split("\n").join('');
+}
