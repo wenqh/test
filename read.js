43a943babed6bda24bc2351da9f0b566e69f9fa9
wenqh <wen-qh@163.com>
2023-04-25 18:27:04 +0800

    Update read.js

--- read.js
+++ read.js
@@ -19,13 +19,15 @@
         alert("脚本执行失败未初始化文档" + document)
     }
 
-    let tool = document.createElement('div');
+    let tool = document.createElement('iframe');
+
     let size = window.innerWidth / 15;
-    tool.style.cssText = `position:fixed;top:70px;right:0;font-size:${size}px;z-index:998;background:#bbbbbb`;
-    tool.innerHTML = `<img src1="${url}?url=${document.location.href}&preload=false" style="display:none"><a href="${url}?url=${document.location.href}">URL</a><form action="${url}" method="post" accept-charset="UTF-8"><input name="url" type="hidden" value="${document.location.href}"><input name="body" type="hidden"><button type="submit" id="_sendtxt">提取文字</button></form><button id="scroll">滚动</button>`;
+    tool.style.cssText = `height: 100px; width: 70px; border: unset; display: flex; position:fixed;top1:70px;bottom: 10%;right:0;font-size:${size}px;z-index:998;background:#bbbbbb`;
+	document.body.appendChild(tool);
+    tool.contentWindow.document.body.innerHTML = `<img src1="${url}?url=${document.location.href}&preload=false" style="display:none"><a href="${url}?url=${document.location.href}">URL</a><form action="${url}" method="post" accept-charset="UTF-8"><input name="url" type="hidden" value="${document.location.href}"><input name="body" type="hidden"><button type="submit" id="_sendtxt">提取文字</button><button id="scroll">滚动</button></form>`;
 
     //document.addEventListener("DOMContentLoaded", (e) => {
-        document.body.appendChild(tool)
+    //    document.body.appendChild(tool)
     //})
 
     tool.querySelector("form").addEventListener("submit", (e) => {
